377172ef1fd9a053e14d8eaf1a7635ad23f0550c
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-08-24 21:33:05 -0400

    creates tree class and with add method

--- binary_tree.rb
+++ binary_tree.rb
@@ -9,3 +9,37 @@ class Node
   end
 
 end
+
+class Tree
+
+  attr_accessor :root
+
+  def initialize
+    @root = nil
+  end
+
+  def add(newNode)
+    if @root.nil?
+      @root = newNode
+    else
+      addNode(newNode, @root)
+    end
+  end
+
+  def addNode(newNode, currentNode)
+    if newNode.value < currentNode.value
+      if currentNode.left.nil?
+        currentNode.left = newNode.value
+      else
+        addNode(newNode, currentNode.left)
+      end
+    else
+      if currentNode.right.nil?
+        currentNode.right = newNode.value
+      else
+        addNode(newNode, currentNode.right)
+      end
+    end
+  end
+
+end
